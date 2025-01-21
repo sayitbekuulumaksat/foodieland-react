3e26c1a1ecbb5de83480b03acd78352d25d0996e
Saiitbekuulu Maksatbek <sayitbekuulumaksat@gmail.com>
2025-01-22 02:54:32 +0600

    add Routes Route for pages navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./pages/Home/Hero";
-import Categories from "./pages/Home/Categories";
-import Recipes from "./pages/Home/Recipes";
-import Chef from "./pages/Home/Chef";
-import CheckOutInstagram from "./pages/Home/CheckOutInstagram";
-import Delicious from "./pages/Home/delicious";
-import Inbox from "./pages/Home/Inbox";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -17,19 +11,12 @@ function App() {
         <Navbar />
       </header>
       <main>
-        {/* <Routes>
-          <Route />
-        </Routes> */}
-        <Hero/>
-        <Categories />
-        <Recipes/>
-        <Chef/>
-        <CheckOutInstagram/>
-        <Delicious/>
-        <Inbox/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </main>
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import Categories from "./Categories";
+import Recipes from "./Recipes";
+import Chef from "./Chef";
+import CheckOutInstagram from "./CheckOutInstagram";
+import Delicious from "./delicious";
+import Inbox from "./Inbox";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Categories />
+      <Recipes />
+      <Chef />
+      <CheckOutInstagram />
+      <Delicious />
+      <Inbox />
+    </>
+  );
+}
+
+export default Home;
