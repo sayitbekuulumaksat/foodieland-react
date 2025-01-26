de7deabb638c4588aaeb4486362e8f160a5b31c3
Saiitbekuulu Maksatbek <sayitbekuulumaksat@gmail.com>
2025-01-27 04:19:36 +0600

    added text to README

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/foodieland-react/",
   plugins: [react(), svgr(), tailwindcss()],
   resolve: {
     alias: {
